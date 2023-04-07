ffea6ac3690dac5fff50edf3ea09460414e12b1f
bruno-oli <brunomaz177@gmail.com>
2023-04-06 23:58:06 -0300

    add react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const [output, setOutput] = useState("");
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
   return (
-    <main className="h-screen bg-zinc-50 flex items-center justify-center">
-      <form className="flex flex-col gap-4 w-full max-w-xs">
+    <main className="h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center">
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className="flex flex-col gap-4 w-full max-w-xs"
+      >
         <div className="flex flex-col gap-1">
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
-            name="email"
             className="border-zinc-200 shadow-sm rounded h-10 px-3"
+            {...register("email")}
           />
         </div>
 
@@ -17,7 +28,7 @@ function App() {
           <label htmlFor="password">Senha</label>
           <input
             type="password"
-            name="password"
+            {...register("password")}
             className="border-zinc-200 shadow-sm rounded h-10 px-3"
           />
         </div>
@@ -29,6 +40,7 @@ function App() {
           Salvar
         </button>
       </form>
+      <pre>{output}</pre>
     </main>
   );
 }
